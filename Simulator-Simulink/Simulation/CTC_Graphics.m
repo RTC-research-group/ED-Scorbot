fa0a78ff91d6791e34a9d5b564ee1e5847e9f53a
titi2194 <71189786+titi2194@users.noreply.github.com>
2024-10-01 20:18:56 +0200

    Add files via upload

---
+++ Simulator-Simulink/Simulation/CTC_Graphics.m
@@ -0,0 +1,91 @@
+%% Load Data
+% Measured trajectory
+x_m = out.m_CTC.Data(:, 1);
+y_m = out.m_CTC.Data(:, 2);    
+z_m = out.m_CTC.Data(:, 3); 
+
+% Reference Trayectory
+x_ref = reshape(out.ref_CTC.Data(1,:), [], 1);
+y_ref = reshape(out.ref_CTC.Data(2,:), [], 1);
+z_ref = reshape(out.ref_CTC.Data(3,:), [], 1);
+
+% Time
+time = out.tout;
+%% Display only the points of the trajectory from time 2 seconds onwards
+t1 = 2;
+init_index = find(time >= t1, 1);  
+time_2s = time(init_index:end);
+n1_CTC = length(time_2s) - 1;
+% Ref points
+x_ref_2s = x_ref(end-n1_CTC:end);
+y_ref_2s = y_ref(end-n1_CTC:end);
+z_ref_2s = z_ref(end-n1_CTC:end);
+% Mesure points
+x_m_2s = x_m(end-n1_CTC:end);
+y_m_2s = y_m(end-n1_CTC:end);
+z_m_2s = z_m(end-n1_CTC:end);
+
+%% Trajectory graph 
+figure;
+hold on; 
+plot3(x_ref_2s, y_ref_2s, z_ref_2s, 'k', 'LineWidth', 1.5);  
+plot3(x_m_2s, y_m_2s, z_m_2s, 'r', 'LineWidth', 1.5);  
+hold off;
+xlabel('x (m)');
+ylabel('Y (m)');
+zlabel('Z (m)');
+grid on;
+axis equal; 
+legend('Reference trajectory', 'CTC');
+view(3); 
+rotate3d on;  
+%% x, y, z graphs starting from 2 seconds
+% depending on the trajectory you can change the "xlim" to see all 
+% generated points. For square trajectory use a maximum "xlim" of 7.5. 
+% For lemniscate trajectory use a maximum "xlim" of 10.
+% X Graphics
+figure;
+subplot(3,1,1);
+plot(time_2s, x_ref_2s, 'k', 'LineWidth', 2);    
+hold on;
+plot(time_2s, x_m_2s, 'r','LineWidth', 2);   
+hold off;
+xlim([2 7.5]);
+ylabel('x (m)');
+grid on;
+legend('reference', 'CTC');
+% Y Graphics
+subplot(3,1,2);
+plot(time_2s, y_ref_2s, 'k', 'LineWidth', 2);    
+hold on;
+plot(time_2s, y_m_2s, 'r','LineWidth', 2);   
+hold off;
+xlim([2 7.5]); 
+ylabel('y (m)');
+grid on;
+legend('reference', 'CTC');
+% Z Graphics
+subplot(3,1,3);
+plot(time_2s, z_ref_2s, 'k', 'LineWidth', 2);   
+hold on;
+plot(time_2s, z_m_2s, 'r','LineWidth', 2);    
+hold off;
+xlim([2 7.5]);
+ylabel('z (m)');
+grid on;
+legend('reference', 'CTC');
+
+%% Eucledian distance
+eucledian_distance = sqrt((x_ref_2s - x_m_2s).^2 + (y_ref_2s - y_m_2s).^2 + (z_ref_2s - z_m_2s).^2);
+figure;
+semilogy(time_2s, eucledian_distance, 'r', 'LineWidth', 2);
+hold on;
+%hold off;
+xlim([2 7.5]);
+xlabel('Time (s)');
+ylabel('Error (m)');
+grid on;
+legend('Error');
+%% RMSE
+rmse = sqrt(mean(eucledian_distance.^2));
+display(rmse);
---
+++ Simulator-Simulink/Simulation/Load_Robot_and_Points.m
@@ -0,0 +1,47 @@
+%% LOAD ROBOT
+EDScorbot = importrobot('EDScorbot_Arm.slx');
+
+%% SQUARE POINTS
+% This is the set of points of a square path
+sqr = [0.472 0.35 0.25 0.25 0.25 0.4 0.4 0.25; 
+    0.033 0.25 0.25 0.4 0.4 0.4 0.25 0.25; 
+    0.6585 0.35 0.3 0.3 0.3 0.3 0.3 0.3];
+
+%% LEMNISCATE POINTS
+a = 0.2;          
+n = 100;
+t = linspace(0, 10, n);
+
+% Lemniscate equations
+y_lemn = 0.033 + (a * cos(t)) ./ (1 + sin(t).^2); 
+x_lemn = 0.05 + (a * sin(t) .* cos(t)) ./ (1 + sin(t).^2);
+z = 0.35 * ones(size(t));
+
+% Centring points of the lemniscate
+P_inicio = [0.4, 0.033];  
+P_final = [0.4, 0.033];   
+
+center = (P_inicio + P_final) / 2;
+dist = norm(P_final - P_inicio);
+
+x = x_lemn + center(1);
+y = y_lemn + center(2);
+
+xyz_points = [x', y', z']';
+time_lemniscate = t;
+
+% Points of the trajectory from the rest position to the starting 
+% point of the lemniscate.
+lemniscate = [0.45 0.45 0.45; 
+    0.2660 0.2660 0.2660; 
+    0.35 0.35 0.35];
+
+% This is the set of points of a Lemniscate path
+lemniscate = [lemniscate, xyz_points];
+
+% Time matrix expasion
+matrix_time_1 = [0.01 0.02 0.03];
+time_lemniscate = [time_lemniscate, zeros(1, 3)];
+time_lemniscate(:, 5:end) = time_lemniscate(:, 2:end-3);  
+time_lemniscate(:, 2:4) = matrix_time_1;
+
